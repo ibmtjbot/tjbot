70af41f729bfa8878e4e5b3092abd5da8d037cb1
Bruno Braga <bgbraga@gmail.com>
2017-03-25 11:25:05 -0300

    node symbolic link

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -7,6 +7,7 @@ sudo apt-get -y dist-upgrade
 #nodejs
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo ln -s "$(which nodejs)" /usr/bin/node
 
 #alsa
 sudo apt-get install -y alsa-base alsa-utils
