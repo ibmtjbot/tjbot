f2741740d78ae4a15f574504e16a4b88fa560e11
Bruno Braga <bgbraga@gmail.com>
2017-03-26 11:03:44 -0300

    First bootstrap version

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 
-#update
+#----validation
+#if $EUID -ne 0; then
+#    echo "You need to install as root by using sudo";
+#    exit
+#fi
+
+#----force ipv4
+if ! grep -q "ipv6.disable=1" /boot/cmdline.txt; then
+    echo " ipv6.disable=1" | sudo tee -a /boot/cmdline.txt
+    echo "We just disabled ipv6 due better network compatibility. It will take effect after restart.";
+fi
+
+#----default DNS (google)
+if ! grep -q "nameserver 8.8.8.8" /etc/resolv.conf; then
+    echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+    echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
+fi
+
+#----debian missing locale
+sudo apt-get install language-pack-en
+export LC_ALL="en_US.UTF-8"
+
+#----update raspberry
 sudo apt-get update
 sudo apt-get -y dist-upgrade
 
-#nodejs
+#----nodejs install
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo ln -s "$(which nodejs)" /usr/bin/node
 
-#alsa
+#----alsa install
 sudo apt-get install -y alsa-base alsa-utils
