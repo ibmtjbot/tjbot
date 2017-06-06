dc8ab40e241f8a4d9cd1c6771b85f1a1657da0e0
Victor D <chuvidi2003@gmail.com>
2017-06-06 16:54:37 -0400

    Add note regarding username/password interchange.

--- recipes/sentiment_analysis/config.default.js
+++ recipes/sentiment_analysis/config.default.js
@@ -1,4 +1,12 @@
-// User-specific configuration
+/*
+User-specific configuration
+    ** IMPORTANT NOTE ********************
+    * Please ensure you do not interchange your username and password.
+    * Hint: Your username is the lengthy value ~ 36 digits including a hyphen
+    * Hint: Your password is the smaller value ~ 12 characters
+*/ 
+
+
 exports.sentiment_keyword = "education";       // keyword to monitor in Twitter
 exports.sentiment_analysis_frequency_sec = 30; // analyze sentiment every N seconds
 
