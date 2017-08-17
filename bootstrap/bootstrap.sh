fb4e44e8176f4c070be737ddf6b86c633dc9816c
Justin Weisz <jweisz@us.ibm.com>
2017-08-17 16:19:31 -0400

    updates to bootstrap to support raspian (stretch), not tested

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -16,16 +16,18 @@ echo "-----------------------------------------------------------------------"
 echo "Welcome! Let's set up your Raspberry Pi with the TJBot software."
 echo ""
 echo "Important: This script was designed for setting up a Raspberry Pi after"
-echo "a clean install of Raspbian (Jessie). If you are running this on a"
+echo "a clean install of Raspbian. If you are running this script on a"
 echo "Raspberry Pi that you've used for other projects, please take a look at"
 echo "what this script does BEFORE running it to ensure you are comfortable"
 echo "with its actions (e.g. performing an OS update, installing software"
 echo "packages, removing old packages, etc.)"
 echo "-----------------------------------------------------------------------"
 
+#----turn off case matching for input
+shopt -s nocasematch
+
 #----confirm bootstrap
 read -p "Would you like to use this Raspberry Pi for TJBot? Y/n: " choice
-shopt -s nocasematch
 case "$choice" in
  "n" )
     echo "OK, TJBot software will not be installed at this time."
@@ -34,30 +36,45 @@ case "$choice" in
  *) ;;
 esac
 
+#----test raspbian version: 8 is jesse, 9 is stretch
+RASPIAN_VERSION=`cat /etc/os-release | grep VERSION_ID | cut -d '"' -f 2`
+if [ $RASPIAN_VERSION -ne 8 ] && [ $RASPIAN_VERSION -ne 9 ]; then
+    echo "Warning: it looks like your Raspberry Pi is not running Raspian (Jesse)"
+    echo "or Raspian (Stretch). TJBot has only been tested on these versions of"
+    echo "Raspian."
+    echo ""
+    read -p "Would you like to continue with setup? Y/n: " choice
+    case "$choice" in
+        "n" )
+            echo "OK, TJBot software will not be installed at this time."
+            exit
+            ;;
+        * ) ;;
+    esac
+fi
+
 #----setting TJBot name
 CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
 echo ""
 echo "Please enter a name for your TJBot. This will be used for the hostname of"
 echo "your Raspberry Pi."
 read -p "TJBot name (current: $CURRENT_HOSTNAME): " name
-shopt -s nocasematch
 if [ -z "${name// }" ]; then
     name=$CURRENT_HOSTNAME
 fi
 echo "Setting DNS hostname to $name"
-echo "$name" | sudo tee /etc/hostname >/dev/null 2>&1
-sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$name/g" /etc/hosts
+echo "$name" | tee /etc/hostname >/dev/null 2>&1
+sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$name/g" /etc/hosts
 
 #----disabling ipv6
 echo ""
 echo "In some networking environments, disabling ipv6 may help your Pi get on"
 echo "the network."
 read -p "Disable ipv6? (y/N): " choice
-shopt -s nocasematch
 case "$choice" in
  "y" )
     echo "Disabling ipv6"
-    echo " ipv6.disable=1" | sudo tee -a /boot/cmdline.txt
+    echo " ipv6.disable=1" | tee -a /boot/cmdline.txt
     echo "ipv6 has been disabled. It will take effect after rebooting.";;
  *) ;;
 esac
@@ -67,13 +84,12 @@ echo ""
 echo "In some networking environments, using Google's nameservers may speed up"
 echo "DNS queries."
 read -p "Enable Google DNS? (y/N): " choice
-shopt -s nocasematch
 case "$choice" in
  "y" )
     echo "Adding Google DNS servers to /etc/resolv.conf"
-    if [ ! grep -q "nameserver 8.8.8.8" /etc/resolv.conf ]; then
-        echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
-        echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
+    if ! grep -q "nameserver 8.8.8.8" /etc/resolv.conf; then
+        echo "nameserver 8.8.8.8" | tee -a /etc/resolv.conf
+        echo "nameserver 8.8.4.4" | tee -a /etc/resolv.conf
     fi ;;
  *) ;;
 esac
@@ -81,13 +97,12 @@ esac
 #----setting local to US
 echo ""
 read -p "Force locale to US English (en-US)? (y/N): " choice
-shopt -s nocasematch
 case "$choice" in
  "y" )
     echo "Forcing locale to en-US. Please ignore any errors below."
     export LC_ALL="en_US.UTF-8"
-    echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
-    sudo locale-gen en_US.UTF-8
+    echo "en_US.UTF-8 UTF-8" | tee -a /etc/locale.gen
+    locale-gen en_US.UTF-8
     ;;
  *) ;;
 esac
@@ -97,7 +112,6 @@ echo ""
 echo "TJBot requires an up-to-date installation of your Raspberry Pi's operating"
 echo "system software."
 read -p "Proceed with apt-get dist-upgrade? (Y/n): " choice
-shopt -s nocasematch
 case "$choice" in
  "n" )
     echo "Warning: you may encounter problems running TJBot recipes without performing"
@@ -106,9 +120,9 @@ case "$choice" in
     ;;
  *)
     echo "Updating apt repositories [apt-get update]"
-    sudo apt-get update
+    apt-get update
     echo "Upgrading OS distribution [apt-get dist-upgrade]"
-    sudo apt-get -y dist-upgrade
+    apt-get -y dist-upgrade
     ;;
 esac
 
@@ -118,11 +132,10 @@ echo ""
 echo "TJBot requires Node.js version 6. We detected Node.js version $node_version"
 echo "is already installed."
 read -p "Install Node 6.x? (Y/n): " choice
-shopt -s nocasematch
 case "$choice" in
  "y" )
-    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-    sudo apt-get install -y nodejs
+    curl -sL https://deb.nodesource.com/setup_6.x | bash -
+    apt-get install -y nodejs
     ;;
  *)
     echo "Warning: TJBot will encounter problems with versions of Node.js older than 6.x."
@@ -132,19 +145,23 @@ esac
 
 #----install additional packages
 echo ""
-echo "Installing additional software packages (alsa, libasound2, git, pigpio)"
-sudo apt-get install -y alsa-base alsa-utils libasound2-dev git pigpio
+if [ $RASPIAN_VERSION -eq 8 ]; then
+    echo "Installing additional software packages for Jesse (alsa, libasound2-dev, git, pigpio)"
+    apt-get install -y alsa-base alsa-utils libasound2-dev git pigpio
+#elif [ $RASPIAN_VERSION -eq 9 ]; then
+#    echo "Installing additional software packages for Stretch (libasound2-dev)"
+#    apt-get install -y libasound2-dev
+fi
 
 #----remove outdated apt packages
 echo ""
 echo "Removing unused software packages [apt-get autoremove]"
-sudo apt-get -y autoremove
+apt-get -y autoremove
 
 #----enable camera on raspbery pi
 echo ""
 echo "If your Raspberry Pi has a camera installed, TJBot can use it to see."
 read -p "Enable camera? (y/N): " choice
-shopt -s nocasematch
 case "$choice" in
  "y" )
     if grep "start_x=1" /boot/config.txt
@@ -154,15 +171,15 @@ case "$choice" in
         echo "Enabling camera."
         if grep "start_x=0" /boot/config.txt
         then
-            sudo sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
+            sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
         else
-            echo "start_x=1" | sudo tee -a /boot/config.txt >/dev/null 2>&1
+            echo "start_x=1" | tee -a /boot/config.txt >/dev/null 2>&1
         fi
         if grep "gpu_mem=128" /boot/config.txt
         then
             :
         else
-            echo "gpu_mem=128" | sudo tee -a /boot/config.txt >/dev/null 2>&1
+            echo "gpu_mem=128" | tee -a /boot/config.txt >/dev/null 2>&1
         fi
     fi
     ;;
@@ -179,7 +196,7 @@ fi
 
 if [ ! -d $TJBOT_DIR ]; then
     echo "Cloning TJBot project to $TJBOT_DIR"
-    git clone https://github.com/ibmtjbot/tjbot.git $TJBOT_DIR
+    sudo -u $SUDO_USER git clone https://github.com/ibmtjbot/tjbot.git $TJBOT_DIR
 else
     echo "TJBot project already exists in $TJBOT_DIR, leaving it alone"
 fi
@@ -193,15 +210,14 @@ echo "be able to play sound and use the LED at the same time. If you plan to use
 echo "the built-in audio jack, we recommend NOT disabling the sound kernel"
 echo "modules."
 read -p "Disable sound kernel modules? (Y/n): " choice
-shopt -s nocasematch
 case "$choice" in
  "y" )
     echo "Disabling the kernel modules for the built-in audio jack."
-    sudo cp $TJBOT_DIR/bootstrap/tjbot-blacklist-snd.conf /etc/modprobe.d/ 
+    cp $TJBOT_DIR/bootstrap/tjbot-blacklist-snd.conf /etc/modprobe.d/
     ;;
  "n" )
     echo "Enabling the kernel modules for the built-in audio jack."
-    sudo rm /etc/modprobe.d/tjbot-blacklist-snd.conf
+    rm /etc/modprobe.d/tjbot-blacklist-snd.conf
     ;;
  *) ;;
 esac
@@ -311,24 +327,22 @@ echo "sound configuration, we recommend rebooting first before running these"
 echo "tests as they may fail. You can run these tests at anytime by running"
 echo "the runTests.sh script in the tjbot/bootstrap folder."
 read -p "Would you like to run hardware tests at this time? (y/N): " choice
-shopt -s nocasematch
 case "$choice" in
- "y" )
-    ./runTests.sh $TJBOT_DIR
-    ;;
- *) ;;
+    "y" )
+        ./runTests.sh $TJBOT_DIR
+        ;;
+    *) ;;
 esac
 
 #----reboot
 echo ""
 read -p "We recommend rebooting for all changes to take effect. Reboot? (Y/n): " choice
-shopt -s nocasematch
 case "$choice" in
- "y" )
-    echo "Rebooting."
-    sudo reboot
-    ;;
- *)
-     echo "Please reboot your Raspberry Pi for all changes to take effect."
-    ;;
+    "y" )
+        echo "Rebooting."
+        reboot
+        ;;
+    *)
+        echo "Please reboot your Raspberry Pi for all changes to take effect."
+        ;;
 esac
