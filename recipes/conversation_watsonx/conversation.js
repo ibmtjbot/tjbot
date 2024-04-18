a5a7fe78e23b2f0a1efe0076566faf6a3c3ab720
Morgan Carroll <modcarroll@gmail.com>
2024-04-18 14:51:39 -0500

    works for RPi 3

--- recipes/conversation_watsonx/conversation.js
+++ recipes/conversation_watsonx/conversation.js
@@ -61,7 +61,7 @@ console.log("Say 'stop' or press ctrl-c to exit this recipe.");
 
 while (true) {
   console.log("Listening");
-  const msg = await tj.listen();
+  let msg = await tj.listen();
 
   // check to see if they are talking to TJBot
   if (msg.toLowerCase().startsWith(config.robotName.toLowerCase())) {
