f231cd9d61403ea3012a3d5d95534ece3e32871a
Justin Weisz <jweisz@us.ibm.com>
2025-08-11 18:20:39 -0400

    updated eslint config and switched the default model to llama 4 maverick

---
+++ recipes/chat_with_tj/eslint.config.js
@@ -0,0 +1,7 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+]);
